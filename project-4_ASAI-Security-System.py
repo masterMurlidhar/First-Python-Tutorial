31f3b609d08dfdc4fbe22d53ec2164df5c231af7
Captain Skywalker <murlidharsingh1319@gmail.com>
2021-08-30 18:40:03 +0530

    Projects

--- project-4_ASAI-Security-System.py
+++ project-4_ASAI-Security-System.py
@@ -1,3 +1,10 @@
+"""
+Developer: Capt. Skywalker
+Purpose: Project-4, ASAI Security System
+Stardate: 12020.--
+
+"""
+
 # users
 Registered_Users = ['Murlidhar', "Sahil", 'ASAI', 'Skywalker', "Captain"]
 
@@ -6,13 +13,13 @@ Admin_Password = "asaiinc"
 
 while True:
     # self-intro
-    print('\n Hello!  I\'m ASAI Security System :) ...')
+    print('\n\n\t Hello,  I\'m ASAI Security System :) ...\n\n')
     # username
-    username = input('Enter Your User-ID\ Username: ').strip().capitalize()
+    username = input('Enter Your User-ID\ Username #_  ').strip().capitalize()
     # verify user
     if username in Registered_Users:
         print(
-            f"Welcome {username}, You Can Proceed Now... ").strip().capitalize()
+            f"\nWelcome aboard {username}, You can proceed now... \n\n")
 
         # ask to proceed further
         query1 = input(
@@ -21,18 +28,21 @@ while True:
         if query1 == 'y':
             # ask to be removed
             remove_user = input(
-                'Would you want to be removed from the system? (y/n); ').strip().lower()
+                'Would you like to be removed from the system? (y/n); ').strip().lower()
             if remove_user == 'y':
 
                 # ask for the password
-                verify_pass = int(input('Enter The Administrator Password; '))
+                Verification_Password = input(
+                    'Enter The Administrator Password #_  ')
 
                 # remove user
-                if verify_pass == password:
-                    official_users.remove(user)
+                if Verification_Password == Admin_Password:
+                    Registered_Users.remove(username)
+                    print(
+                        "\nSuccessful :)  You are now removed from the registered users list...\n")
 
                 else:
-                    print('Sorry!  You\'ve Entered Wrong Password :( ')
+                    print('\nFailed :(  You\'ve Entered Wrong Password :( \n')
                     continue
 
         else:
@@ -40,27 +50,28 @@ while True:
 
     # if user not in list
     else:
-        print('Sorry!, I Didn\'t Recognized You :( \n')
+        print('\nFailed :( I Didn\'t Recognized You :( \n\n')
 
         # ask to proceed further
         query2 = input(
             'Would you like to proceed further? (y/n); ').strip().lower()
         if query2 == 'y':
-
             # ask to be added
             update_user = input(
-                'Would you want to be added to the system? (y/n); ').strip().lower()
+                'Would you like to be added to the system? (y/n); ').strip().lower()
             if update_user == 'y':
-
                 # ask for the password
-                verify_pass = int(input('Enter The Administrator Password; '))
-
+                Verification_Password = input(
+                    'Enter The Administrator Password #_   ')
                 # update user
-                if verify_pass == password:
-                    official_users.append(user)
+                if Verification_Password == Admin_Password:
+                    Registered_Users.append(username)
+                    print(
+                        "\nSuccessful :)  You are now added in the registered users list...\n")
 
                 else:
-                    print('Sorry!  You\'ve Entered Wrong Password :( ')
+                    print(
+                        '\nFailed :(  You\'ve entered a wrong password, This incident will be reported :( ... \n')
                     continue
 
         else:
